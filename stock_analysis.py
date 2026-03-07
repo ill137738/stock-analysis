25e33507eb0279c1a08c06a9a6ab508af0c11cf2
ill137738 <ill137738@gmail.com>
2026-03-07 21:34:52 +0900

    Update stock_analysis.py

--- stock_analysis.py
+++ stock_analysis.py
@@ -175,7 +175,8 @@ def send_telegram(message):
     payload = {
         "chat_id": TELEGRAM_CHAT_ID,
         "text": message,
-        "parse_mode": "HTML"
+        "parse_mode": "HTML",
+        "disable_web_page_preview": True
     }
 
     try:
