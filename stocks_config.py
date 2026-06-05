2fa03cf3243eba53b64d88a22b45d66b37b6fb9e
ill137738 <ill137738@gmail.com>
2026-06-05 13:14:51 +0900

    Update stocks_config.py

--- stocks_config.py
+++ stocks_config.py
@@ -11,6 +11,7 @@ STOCKS = {
     "AMD": "AMD stock",
     "Intell": "INTC stock",
     "Arm Holdings": "Arm stock",
+    "SandisK": "SNDK stock",
     "META": "META stock",
     "CoreWeave": "CoreWeave CRWV stock",
     "이더리움": "ethereum",
